3c69f79887866bc720337240243c22f653bfec69
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2025-01-06 18:41:07 +0100

    add: example code in test files

--- test.jsx
+++ test.jsx
@@ -1,1 +1,8 @@
 // this file exists just so that react linting is actually ran
+const React = require("react");
+
+function TestComponent({ name }) {
+  return <p>Hello, {name || "World"}!</p>;
+}
+
+module.exports = { TestComponent };
--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
 // this file exists just so that typescript linting is actually ran
+
+const nums = Array<number>(25)
+  .fill(0)
+  .map((_, i) => i);
+const map = new Map<"even" | "uneven", number[]>();
+
+for (const num of nums) {
+  const key = num % 2 == 1 ? "uneven" : "even";
+  let arr = map.get(key);
+  if (!arr) {
+    arr = [];
+    map.set(key, arr);
+  }
+  arr.push(num);
+}
+
+// funfact for those actually looking at this file: Map.groupBy is supported now, use that instead of doing a loop like above
